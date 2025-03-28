07a098ee2ee729a3be2fb9ea06bd5b18bf7306b0
shota-nakajima <diginarublerebo@yahoo.co.jp>
2025-03-28 12:03:20 +0900

    初期コミット：ToDoアプリのセットアップ

---
+++ backend/todos/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from .models import Todo
+from .serializers import TodoSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all().order_by('-created_at')
+    serializer_class = TodoSerializer
+
+# Create your views here.
